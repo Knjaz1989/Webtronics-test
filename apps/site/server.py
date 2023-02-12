2cc662671f30afb225054a8cdfe89abcdeb57604
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-12 15:41:26 +0300

    server.py was changed

--- apps/site/server.py
+++ apps/site/server.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 from apps.site.user.route import user_router
+from database.db_connection import db
 
 
 app = FastAPI()
-
-
 app.include_router(user_router)
 
+# app.mount(
+#     "/static",
+#     StaticFiles(directory="apps/site/static/"),
+#     name="static",
+# )
 
-app.mount(
-    "/static",
-    StaticFiles(directory="apps/site/static/"),
-    name="static",
-)
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
