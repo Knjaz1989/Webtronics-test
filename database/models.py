86ffab83402e1a5f36e6183b8ddebb8d4cc331d1
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-12 15:35:37 +0300

    models was changed

---
+++ database/models.py
@@ -0,0 +1,52 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import declarative_base, validates
+
+from apps.site.utils.helpers import check_email
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+    name = sa.Column(sa.String(40), nullable=False)
+    hashed_password = sa.Column(sa.String(), nullable=False)
+    email = sa.Column(sa.String(50), unique=True, index=True)
+    is_active = sa.Column(
+        sa.Boolean,
+        server_default=sa.sql.expression.true(),
+        nullable=False,
+    )
+
+    def __str__(self):
+        return f'{self.login}: {self.email}'
+
+    def __repr__(self):
+        return f'{self.login}: {self.email}'
+
+
+class Token(Base):
+    __tablename__ = 'tokens'
+
+    id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+    token = sa.Column(sa.String)
+    expires = sa.Column(sa.DateTime)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey(User.id, ondelete='CASCADE'))
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+    title = sa.Column(sa.VARCHAR(150))
+    text = sa.Column(sa.TEXT)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey(User.id, ondelete='CASCADE'))
+    likes = sa.Column(sa.Integer, default=0)
+    dislikes = sa.Column(sa.Integer, default=0)
+
+    def __str__(self):
+        return f'{self.login}: {self.email}'
+
+    def __repr__(self):
+        return f'{self.login}: {self.email}'
