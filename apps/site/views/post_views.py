20d7855123987949f44f5cbb41bdbe24ea615094
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-17 16:34:57 +0300

    add dependency, refactor post view

--- apps/site/views/post_views.py
+++ apps/site/views/post_views.py
@@ -1,37 +1,37 @@
 from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer
 
 from apps.site.utils import helpers as hls
-from apps.site.schemas.post_schemas import PostAdd, PostDelete, PostUpdate
+from apps.site.schemas.post_schemas import PostAdd, PostDelete, PostUpdate, \
+    PostBase
+from apps.site.utils.dependencies import get_user
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-
-async def add_post(post: PostAdd, token: str = Depends(oauth2_scheme)):
-    data = hls.decode_token(token)
-    db_user = await hls.get_user_by_email(data.get("email"))
-    await hls.create_post(user_id=db_user.get("id"), data=post.dict())
+async def add_post(post: PostAdd, user: dict = Depends(get_user)):
+    await hls.create_post(user_id=user.get("id"), data=post.dict())
     return {"status": "Success", "msg": "The post was created successfully"}
 
 
-async def get_all_posts(token: str = Depends(oauth2_scheme)):
-    hls.decode_token(token)
+async def get_all_posts(user: dict = Depends(get_user)):
     posts = await hls.get_posts()
     return {"status": "Success", "data": posts}
 
 
-async def delete_post(post_data: PostDelete,
-                      token: str = Depends(oauth2_scheme)):
-    data = hls.decode_token(token)
-    db_user = await hls.get_user_by_email(data.get("email"))
-    await hls.delete_post(db_user.get("id"), post_data.post_id)
+async def delete_post(post_data: PostDelete, user: dict = Depends(get_user)):
+    await hls.delete_post(user.get("id"), post_data.post_id)
     return {"status": "Success", "msg": 'The post was deleted successfully'}
 
 
-async def change_post(post_data: PostUpdate, token: str = Depends(oauth2_scheme)):
-    """Update post in the database"""
-    data = hls.decode_token(token)
-    db_user = await hls.get_user_by_email(data.get("email"))
-    await hls.change_post(db_user.get("id"), post_data.dict())
+async def change_post(
+        post_data: PostUpdate, user: dict = Depends(get_user)
+):
+    await hls.change_post(user.get("id"), post_data.dict())
     return {"status": "Success", "msg": 'The post was changed successfully'}
+
+
+async def paste_like(post: PostBase, user: dict = Depends(get_user)):
+    await hls.rate_post(rate_type='like')
+    return {"status": "Success", "msg": 'You paste like successfully'}
+
+
+def paste_dislike():
+    return {"status": "Success", "msg": 'You paste dislike successfully'}
