7f555de1a595d0bcf7f7e0aac26fc26094a0d2b9
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-17 12:10:16 +0300

    post schema was changed

--- apps/site/schemas/post_schemas.py
+++ apps/site/schemas/post_schemas.py
@@ -1,6 +1,4 @@
-from typing import Dict
-
-from pydantic import BaseModel, Field, validator, root_validator
+from pydantic import BaseModel, Field, root_validator
 
 
 class PostAdd(BaseModel):
@@ -8,13 +6,17 @@ class PostAdd(BaseModel):
     text: str = Field(..., min_length=1)
 
 
-class PostUpdate(BaseModel):
+class PostBase(BaseModel):
     id: int = Field(..., ge=1, alias="post_id")
+
+
+class PostUpdate(PostBase):
+    """Inherit 'id' field from parent"""
     title: str = Field(None, min_length=1)
     text: str = Field(None, min_length=1)
 
     @root_validator()
-    def check(cls, values):
+    def check_fields(cls, values):
         if not values.get('title') and not values.get('text'):
             raise ValueError(
                 "Expected two fields or one of 'title' or 'text'")
