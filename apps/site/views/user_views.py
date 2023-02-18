db487d70a21384d22f7848c80929c71d1abd1fb1
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-18 13:24:25 +0300

    change and fix user views

--- apps/site/views/user_views.py
+++ apps/site/views/user_views.py
@@ -1,19 +1,21 @@
 from fastapi import HTTPException, status
 
-from apps.site.utils import helpers as hls
+from apps.site.utils import helpers as hls, db_handlers as db_h, jwt_token
 from apps.site.schemas.user_schemas import UserCreate, UserLogin
 
 
 async def sign_up(user: UserCreate):
-    db_user = await hls.get_user_by_email(email=user.email)
+    db_user = await db_h.get_user_by_email(email=user.email)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail='Email already registered.')
-    return await hls.create_user(user=user)
+    hash_password = hls.get_hash_password(user.password)
+    await db_h.create_user(user.name, hash_password, user.email)
+    return {"status": "Success", "msg": "The user was created successfully"}
 
 
 async def login(user: UserLogin):
-    db_user = await hls.get_user_by_email(user.email)
+    db_user = await db_h.get_user_by_email(user.email)
     if not db_user or \
             not hls.validate_password(
                 user.password, db_user.get("hashed_password")
@@ -24,5 +26,5 @@ async def login(user: UserLogin):
             headers={"WWW-Authenticate": "Bearer"},
         )
     user.password = hls.get_hash_password(user.password)
-    access_token, expires = hls.create_token(data=user.dict())
+    access_token, expires = jwt_token.create_token(data=user.dict())
     return {"access_token": access_token, "expires": expires}
