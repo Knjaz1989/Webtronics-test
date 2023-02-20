632ccc6902a272e32999fe7bd472020f7811dba6
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-20 20:40:06 +0300

    fix dependeny and migrate version

--- apps/site/utils/dependencies.py
+++ apps/site/utils/dependencies.py
@@ -1,12 +1,19 @@
+from fastapi import HTTPException, status
+
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
-from . import helpers as hls, db_handlers as db_h, jwt_token
+from . import db_handlers as db_h, jwt_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 async def get_user(token: str = Depends(oauth2_scheme)):
     user = jwt_token.decode_token(token)
-    db_user = db_h.get_user_by_email(user.get("email"))
-    return db_user
+    db_user = await db_h.get_user_by_email(user.get("email"))
+    if db_user:
+        return db_user
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="There is no such user with this token",
+    )
