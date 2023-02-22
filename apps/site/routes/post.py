1700529bf05e2d7c097b99955181fe52550ccfb3
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-22 09:54:09 +0300

    change post route position

--- apps/site/routes/post.py
+++ apps/site/routes/post.py
@@ -2,15 +2,18 @@ from fastapi import APIRouter
 
 from apps.site.views import post as pv
 
+
 post_router = APIRouter(prefix="/post", tags=["post"])
 
-post_router.add_api_route("/create", endpoint=pv.add_post, methods=["POST"])
+
+post_router.add_api_route("/", endpoint=pv.get_current_post, methods=["GET"])
+post_router.add_api_route("/", endpoint=pv.add_post, methods=["POST"])
+post_router.add_api_route("/", endpoint=pv.delete_post, methods=["DELETE"])
+post_router.add_api_route("/", endpoint=pv.change_post, methods=["PATCH"])
+
 post_router.add_api_route("/all", endpoint=pv.get_all_posts, methods=["GET"])
-post_router.add_api_route(
-    "/current", endpoint=pv.get_current_post, methods=["GET"]
-)
-post_router.add_api_route("/delete", endpoint=pv.delete_post, methods=["POST"])
-post_router.add_api_route("/update", endpoint=pv.change_post, methods=["POST"])
-post_router.add_api_route("/rate", endpoint=pv.rate_post, methods=["POST"])
-post_router.add_api_route("/unrate", endpoint=pv.unrate_post, methods=["POST"])
 post_router.add_api_route("/search", endpoint=pv.search_post, methods=["GET"])
+
+post_router.add_api_route("/rate", endpoint=pv.rate_post, methods=["POST"])
+post_router.add_api_route("/rate", endpoint=pv.unrate_post, methods=["DELETE"])
+
