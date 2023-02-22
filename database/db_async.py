370459f51c491da9f6d12e44b4ba6cabc2385943
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-22 19:19:04 +0300

    connect within sqlalchemy

--- database/db_async.py
+++ database/db_async.py
@@ -1,25 +1,18 @@
-from databases import Database
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, \
+    async_sessionmaker
 
 from settings import config
 
 
-DATABASE_URL = config.ASYNC_SQLALCHEMY_URL
-
-db = Database(DATABASE_URL, force_rollback=False)
+engine = create_async_engine(config.ASYNC_SQLALCHEMY_URL, echo=True)
 
 
-# engine = create_async_engine(
-#     config.ASYNC_SQLALCHEMY_URL,
-#     echo=True,
-#     future=True,
-# )
-# async_session = sessionmaker(
-#     engine, class_=AsyncSession, expire_on_commit=False
-# )
-# db = async_session
-#
-# async def get_session() -> AsyncSession:
-#     async with async_session() as session:
-#         yield session
+async_session = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_async_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
+        await session.commit()
