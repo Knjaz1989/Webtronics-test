6c02536b5804a7feeb2d00b2ee49ca059f245864
Igor Sverchkov <knjaz1989@gmail.com>
2023-02-25 14:09:36 +0300

    add response model for add post

--- apps/posts/routes.py
+++ apps/posts/routes.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter
 
 from apps.posts import views as pv
+from apps.posts.schemas import PostAddResponse
+
 
 post_router = APIRouter(prefix="/post", tags=["post"])
 
 
 post_router.add_api_route("/", endpoint=pv.get_current_post, methods=["GET"])
-post_router.add_api_route("/", endpoint=pv.add_post, methods=["POST"])
+post_router.add_api_route(
+    path="/", endpoint=pv.add_post, methods=["POST"],
+    response_model=PostAddResponse
+)
 post_router.add_api_route("/", endpoint=pv.delete_post, methods=["DELETE"])
 post_router.add_api_route("/", endpoint=pv.change_post, methods=["PATCH"])
 
@@ -15,4 +20,3 @@ post_router.add_api_route("/search", endpoint=pv.search_post, methods=["GET"])
 
 post_router.add_api_route("/rate", endpoint=pv.rate_post, methods=["POST"])
 post_router.add_api_route("/rate", endpoint=pv.unrate_post, methods=["DELETE"])
-
